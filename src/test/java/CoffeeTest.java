96d764656dea2f5036cebcdcd70740a463970a2d
DuBori <92709439+DuBori@users.noreply.github.com>
2023-08-16 22:34:01 +0900

    step3 : add abstract class upcast and downcast

--- src/test/java/CoffeeTest.java
+++ src/test/java/CoffeeTest.java
@@ -2,6 +2,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CoffeeTest {
     public static void main(String[] args) {
-        new Coffee();
+        Beverage beverage = new Coffee();
+        if (beverage  instanceof Coffee) {
+            Coffee coffee = (Coffee)beverage;
+        }
+        // 인스턴스가 실제로 어떤 타입인지를 확인할 때 사용하는 자바 keyword
     }
 }
--- src/test/java/TeaTest.java
+++ src/test/java/TeaTest.java
@@ -2,6 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TeaTest {
     public static void main(String[] args) {
-        new Tea();
+        Beverage beverage = new Tea();
+
     }
 }
